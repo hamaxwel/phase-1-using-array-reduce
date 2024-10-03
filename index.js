fe005fc8ffa973fff40dd5b9ab88bbecc750c0e5
maxwel kirimi <maxwelkirimii@gmail.com>
2024-10-03 13:42:51 +0300

    document structure

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+// Use reduce to sum up all the values in the array
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue; // accumulate the sum of the batteries
+}, 0); // initial value for the accumulator is 0
+
+console.log(totalBatteries); // Should log 36
+
